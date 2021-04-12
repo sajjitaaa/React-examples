9cddc082967d67906a26725c352f96b019d52593
sajjitaaa <bsajjita@gmail.com>
2021-04-12 17:08:04 +0530

    useState object

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Setup from "./tutorial/1-useState/setup/2-useState-basics";
-// import Setup1 from "./tutorial/1-useState/setup/3-useState-array";
-// import Setup2 from "./tutorial/1-useState/setup/4-useState-object";
+import Setup1 from "./tutorial/1-useState/setup/3-useState-array";
+import Setup2 from "./tutorial/1-useState/setup/4-useState-object";
 // import Setup3 from "./tutorial/1-useState/setup/5-useState-counter";
 // import Setup4 from "./tutorial/2-useEffect/setup/1-useEffect-basics";
 
@@ -14,10 +14,10 @@ function App() {
       <Setup1 />
       <hr />
       <br />
-      {/* <Setup2 />
+      <Setup2 />
       <hr />
       <br />
-      <Setup3 />
+      {/* <Setup3 />
       <hr />
       <hr />
       <br />
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,27 +1,27 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 
-// const UseStateObject = () => {
-//   const [person, setPerson] = useState({
-//     name: "sajjita",
-//     age: 21,
-//     message: "random message",
-//   });
+const UseStateObject = () => {
+  const [person, setPerson] = useState({
+    name: "sajjita",
+    age: 21,
+    message: "random message",
+  });
 
-//   const changeMessage = () => {
-//     setPerson({ ...person, message: "hello " + person.name });
-//   };
-//   return (
-//     <>
-//       <h3>useState object</h3>
-//       <br />
-//       <h2>{person.name}</h2>
-//       <h3>{person.age}</h3>
-//       <h3>{person.message}</h3>
-//       <button className="btn" onClick={changeMessage}>
-//         change message
-//       </button>
-//     </>
-//   );
-// };
+  const changeMessage = () => {
+    setPerson({ ...person, message: "hello " + person.name });
+  };
+  return (
+    <>
+      <h3>useState object</h3>
+      <br />
+      <h2>{person.name}</h2>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
+};
 
-// export default UseStateObject;
+export default UseStateObject;
